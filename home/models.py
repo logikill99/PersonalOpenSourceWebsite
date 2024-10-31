f7ce2aff2b0664185ce1c6133ad0db7474adacad
logikill99 <logikill17@gmail.com>
2024-10-30 22:20:58 -0500

    made changes to about me

--- home/models.py
+++ home/models.py
@@ -1,5 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
 class MyModel(models.Model):
     pass
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    proficiency = models.CharField(max_length=30)
+    description = models.TextField()
+    type = models.CharField(max_length=15)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    title = models.CharField(max_length=50)
+    organization = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    current = models.BooleanField()
+    description = models.TextField()
+    type = models.CharField(max_length=15)
+    skills = models.ManyToManyField("Skill")
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+# TODO: add models for skills, education experience, work experience
--- home/views.py
+++ home/views.py
@@ -1,13 +1,38 @@
+import time
+
 from django.shortcuts import render
 from contactme.models import Contact, Message
 from contactme.views import contact_view, contact_success_view
 from blog.models import Category, Post
-from PersonalHomePage.settings import LISTED_EMAIL, LISTED_GITHUB, LISTED_LINKEDIN, LISTED_TWITTER, LISTED_DISCORD
+from PersonalHomePage.settings import (
+    LISTED_EMAIL,
+    LISTED_GITHUB,
+    LISTED_LINKEDIN,
+    LISTED_TWITTER,
+    LISTED_DISCORD,
+)
+from home.models import Experience, Skill
 
 
 def home(request):
     projects = Post.objects.filter(categories__name__contains="project").order_by(
         "-created_on"
     )
-    return render(request, "index.html", {"projects": projects, "linkedin": LISTED_LINKEDIN, "github": LISTED_GITHUB,
-                                          "twitter": LISTED_TWITTER, "discord": LISTED_DISCORD, "email": LISTED_EMAIL})
+    languages = Skill.objects.filter(type="language")
+    frameworks = Skill.objects.filter(type="framework")
+    hobbies = Skill.objects.filter(type="hobby")
+    return render(
+        request,
+        "index.html",
+        {
+            "projects": projects,
+            "languages": languages,
+            "frameworks": frameworks,
+            "hobbies": hobbies,
+            "linkedin": LISTED_LINKEDIN,
+            "github": LISTED_GITHUB,
+            "twitter": LISTED_TWITTER,
+            "discord": LISTED_DISCORD,
+            "email": LISTED_EMAIL,
+        },
+    )
